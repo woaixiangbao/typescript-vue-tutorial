d119026aca88a36974ed68168015dc66f05bbf3f
jianqing <woaixiangbao@gmail.com>
2019-08-15 11:08:20 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+
+let v = new Vue({
+  el: '#app',
+  template: `
+    <div>
+      <div>
+        Hello {{name}}!
+      </div>
+      Name: <input v-model="name" type="text" />
+    </div>
+  `,
+  data: {
+    name: 'World'
+  }
+})
